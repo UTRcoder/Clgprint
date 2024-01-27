1f522523018234949ac7e3309e02e986f9eab0a9
Utkarsh Rai <100745741+UTRcoder@users.noreply.github.com>
2024-01-27 18:00:54 +0530

    Update Navbar.js

--- clgprint/src/Components/Navbar.js
+++ clgprint/src/Components/Navbar.js
@@ -22,7 +22,7 @@ export default function Navbar(props) {
         <div>
             <nav className="navbar navbar-expand-lg navbar-dark bg-success">
                 <div className="container-fluid">
-                    <Link className="navbar-brand fs-2 fst-italic fst-bold" to="/">ClgPrint</Link>
+                    <Link className="navbar-brand fs-2 fst-italic fst-bold" to="/">FoodiesStore</Link>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
