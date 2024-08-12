44cad035417b9182d808bef471812f79c9902fd7
Utkarsh Rai <100745741+UTRcoder@users.noreply.github.com>
2024-08-12 21:27:54 +0530

    Update Login.js

--- clgprint/src/screens/Login.js
+++ clgprint/src/screens/Login.js
@@ -41,12 +41,12 @@ export default function Login() {
       <div className='container'>
         <form className='w-50 m-auto mt-5 p-3 border bg-dark border-success rounded' onSubmit={handleSubmit}>
           <div className="m-3">
-                        <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
+                        <label htmlFor="exampleInputEmail1" className="form-label text-white">Email address</label>
                         <input type="email" className="form-control" name='email' value={credentials.email} onChange={onChange} />
                         <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
                     </div>
                     <div className="mb-3">
-                        <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
+                        <label htmlFor="exampleInputPassword1" className="form-label text-white">Password</label>
                         <input type="password" className="form-control" id="exampleInputPassword1" name='password' value={credentials.password} onChange={onChange} />
                     </div>
                     <button type="submit" className="btn btn-success">Submit</button>
