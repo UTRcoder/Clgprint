e07b9dbbe50c56aed7bbe81df41c71af9f35d62c
Utkarsh Rai <100745741+UTRcoder@users.noreply.github.com>
2024-08-12 21:48:59 +0530

    Update Cart.js

--- clgprint/src/screens/Cart.js
+++ clgprint/src/screens/Cart.js
@@ -14,8 +14,12 @@ export default function Cart() {
                 <div className='m-5 w-100 text-center fs-3'>The Cart is Empty!</div>
             </div>
         )
-    }
 
+    }
+    let totalPrice = data.reduce((total, food) => total + food.price, 0)
+    var link = "upi://pay?pa=utkarshrai146@oksbi%26am=" + totalPrice + "%26cu=INR%26aid=uGICAgICXtpSdRw";
+    var upi = "https://qrcode.tec-it.com/API/QRCode?data=" + link;
+    let odi = randomNumberInRange(10090009, 20000909);
     const handleCheckOut = async () => {
         let response = await fetch("https://go-food-dgq7.onrender.com/api/orderData", {
             method: 'POST',
@@ -30,18 +34,16 @@ export default function Cart() {
         });
         console.log("JSON RESPONSE:::::", response)
         if (response.status === 200) {
+            alert("Verifying your Payment, Check your mail for Confirmation\nYour Order_ID is:" + odi)
             dispatch({ type: "DROP" })
         }
     }
-
-
-    let totalPrice = data.reduce((total, food) => total + food.price, 0)
 
     return (
         <div>
             <div className='container m-auto mt-5 text-white table-responsive table-responsive-sm table-responsive-md'>
                 <table class="table table-hover">
-                    <thead className='text-white fs-4'>
+                    <thead className='text-success fs-4'>
                         <tr>
                             <th scope="col">#</th>
                             <th scope="col">Name</th>
@@ -59,17 +61,26 @@ export default function Cart() {
                                 <td className='text-white'>{food.qty}</td>
                                 <td className='text-white'>{food.size}</td>
                                 <td className='text-white'>{food.price}</td>
-                                <td className='text-white'><button type="button" className="btn p-0 bg-white"><img src={trash} alt='delete' onClick={() => { dispatch({ type: "REMOVE", index: index }) }} /></button>
+                                <td className='text-white' ><button type="button" className="btn p-0 bg-white"><img src={trash} alt='delete' onClick={() => { dispatch({ type: "REMOVE", index: index }) }} /></button>
                                 </td>
                             </tr>
                         ))}
                     </tbody>
                 </table>
-                <div><h1 className='fs-2 text-white'>Total Price: {totalPrice}/-</h1></div>
-                <div>
-                    <button className='btn bg-success text-white mt-5'onClick={handleCheckOut}> Check Out </button>
+                <div><h1 className='fs-2'>Total Price: {totalPrice}/-</h1></div>
+                <div style={{ textAlign: "center" }}>
+                    <div>
+                        <h1>Scan here to Pay the Amount: </h1>
+                        <img className={upi} src={upi} style={{height:"80px"}}/>
+                        <form style={{ display: " flex", justifyContent: "center", alignItems: " center", paddingTop: "20px" }}>
+                            <label htmlFor="exampleInputtext" className="form-label">Enter UTR/Ref_no here:</label>
+                            <input type="text" className="form-control" name='utr' style={{ width: "50px" }} />
+                        </form>
+                    </div>
+                    <div>
+                        <button className='btn bg-success mt-5' onClick={handleCheckOut}> Check Out </button>
+                    </div>
                 </div>
             </div>
         </div>
     )
-}
